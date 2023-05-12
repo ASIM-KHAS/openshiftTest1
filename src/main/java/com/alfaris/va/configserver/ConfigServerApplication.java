413783c70355a3ab088aaee57cf5baa3645a8113
ASIM KHAS <asimkhas000@gmail.com>
2023-05-12 14:16:17 +0530

    Add files via upload
    uploaded files

---
+++ src/main/java/com/alfaris/va/configserver/ConfigServerApplication.java
@@ -0,0 +1,16 @@
+package com.alfaris.va.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication extends SpringBootServletInitializer{
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
